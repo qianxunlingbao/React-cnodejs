9366099c9c97169cf635cde9c2d33f48b636d6d1
qianxunlingbao <1433917529@qq.com>
2019-11-07 19:21:04 +0800

    add.

---
+++ test/precache-manifest.d41f226f366cb01f2b1b4dbefeebf734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ada6360d015464f1e69085c6e708152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daf3bc91471c0dc449e8",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "2ef4cf848a743a707b2a",
+    "url": "/static/css/main.c01eca68.chunk.css"
+  },
+  {
+    "revision": "daf3bc91471c0dc449e8",
+    "url": "/static/js/2.1f4484bb.chunk.js"
+  },
+  {
+    "revision": "2ef4cf848a743a707b2a",
+    "url": "/static/js/main.779d2bbc.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
